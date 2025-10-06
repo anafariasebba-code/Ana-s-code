982f609d6558e9b49da7f8941b2c4ea962a14e85
anafariasebba-code <anafariasebba@gmail.com>
2025-10-06 15:21:35 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.write("Hello mundo")
-st.image("https://i.pinimg.com/736x/7d/05/2a/7d052ad6217f55ad69b1148326467f8c.jpg"image, caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", *, use_container_width=None)
+st.image("https://i.pinimg.com/736x/7d/05/2a/7d052ad6217f55ad69b1148326467f8c.jpg", caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", *, use_container_width=None)

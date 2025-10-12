78b8bfc4c0b1c319bf70135a65c3bb819b233888
anafariasebba-code <anafariasebba@gmail.com>
2025-10-12 12:03:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,3 @@
 import streamlit as st
-st.write("Welcome to Ana's first code!")
+st.write("Welcome to Ana's world in code!")
 st.image("https://share.google/images/zsHsrl0OmFJ6RN6Su", caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)

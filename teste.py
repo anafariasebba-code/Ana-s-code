5d6845d1a9b9df90f7db90605a20948fa95668fa
anafariasebba-code <anafariasebba@gmail.com>
2025-10-12 19:27:21 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.write("Welcome to Ana's world in code!")
-st.image("https://share.google/images/zsHsrl0OmFJ6RN6Su", caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
+st.image("https://www.gettyimages.com.br/detail/v%C3%ADdeo/3d-animation-cartoon-pink-world-on-white-background-imagens-de-arquivo/2111095304", caption=None, width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)

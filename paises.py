883d92733a821586923ae3abc01bb5215bd08305
anafariasebba-code <anafariasebba@gmail.com>
2025-10-13 15:11:28 -0300

    Update paises.py

--- paises.py
+++ paises.py
@@ -8,4 +8,4 @@ st.dataframe(dataset)
 
 fig = px.choropleth(dataset, locations=dataset ['iso3'], color=dataset ['nome'], hover_name=dataset ['nome'])
 fig.update_layout(title='Mapa Coroléptico dos paises', geo_scope='world')
-st.plotly_chart(fig, use_container_width)
+st.plotly_chart(fig, use_container_width=True, theme='streamlit')
